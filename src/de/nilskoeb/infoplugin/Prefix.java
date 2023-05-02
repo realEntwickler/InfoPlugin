515b5cecbfbe9416a27203355baeab75c46f9d48
Nils Kevin Körting-Eberhardt <nils@familieeberhardt.de>
2023-05-02 09:20:59 +0200

    added Prefix.java for specific message handles

---
+++ src/de/nilskoeb/infoplugin/Prefix.java
@@ -0,0 +1,7 @@
+package de.nilskoeb.infoplugin;
+
+public class Prefix {
+
+    public static String GENERAL = "§eInfoPlugin §8» §7";
+    public static String CMD_NOT_PLAYER = GENERAL + "Du musst ein §eSpieler §7sein, um diesen Command ausführen zu dürfen.";
+}
